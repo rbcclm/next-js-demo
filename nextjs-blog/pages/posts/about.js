ed91723458bc918b2ffc35aae1cca198560c833b
Rebecca Lim <rebecca.lim@phntms.com>
2023-04-25 16:57:32 +0100

    css styling modules

--- nextjs-blog/pages/posts/about.js
+++ nextjs-blog/pages/posts/about.js
@@ -1,16 +1,19 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
     <>
-      <Head>
-        <title>About | Next JS Demo</title>
-      </Head>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
+      <Layout>
+        <Head>
+          <title>About | Next JS Demo</title>
+        </Head>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">Back to home</Link>
+        </h2>
+      </Layout>
     </>
   );
 }
